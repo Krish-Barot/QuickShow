f7b8437b6411be3669546845aaec00f7254cec9d
Krish Barot <HP@mac.home>
2025-09-20 17:21:17 -0400

    Update nodemailer.js

--- server/configs/nodeMailer.js
+++ server/configs/nodeMailer.js
@@ -9,7 +9,7 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-const sendEmail = async ({to, subject, body}) => {
+const sendEmail = async ({to, subject, html}) => {
     const response = await transporter.sendMail({
         from: process.env.SENDER_EMAIL,
         to,
