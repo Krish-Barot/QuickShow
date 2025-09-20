6b80efd0bbb85c2ee674efb35d4d11ba61940695
Krish Barot <HP@mac.home>
2025-09-20 16:07:39 -0400

    Stripe Added Successfully

--- server/controllers/stripeWebHooks.js
+++ server/controllers/stripeWebHooks.js
@@ -14,29 +14,14 @@ export const stripeWebHooks = async (req, res) => {
     let event;
     const secretsEnv = process.env.STRIPE_WEBHOOK_SECRET || '';
 
-    // const secrets = secretsEnv.split(',').map(s => s.trim()).filter(Boolean);
-    // let verifiedWithIndex = -1;
     event = stripeInstance.webhooks.constructEvent(
                 req.body,
                 sig,
                 secretsEnv
             )
 
-    // for (let i = 0; i < secretsEnv.length; i++) {
-    //     const secret = secretsEnv[i];
-    //     try {
-    //         ;
-    //         verifiedWithIndex = i;
-    //         break; 
-    //     } catch (err) {
-    //         continue;
-    //     }
-    // }
-
     if (!event) {
-        console.error('Webhook signature verification failed for all provided secrets. Count =', secretsEnv.length);
-        const masked = secrets.map(s => (s && s.length >= 8) ? (s.slice(0, 7) + '***') : 'invalid');
-        console.error('Provided secrets (masked):', masked);
+        console.error('Webhook signature verification failed for all provided secrets.');
         return res.status(400).send('Webhook Error: signature verification failed');
     }
 
